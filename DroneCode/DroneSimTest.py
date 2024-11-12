89e0e8f6d5e0d660be84153d90aed92c3e484434
RushiMushi <rushi.kona@gmail.com>
2024-11-12 11:36:39 -0600

    fix: remove comment

--- DroneCode/DroneSimTest.py
+++ DroneCode/DroneSimTest.py
@@ -77,41 +77,6 @@ def takeoff_drone(vehicle, targetAltitude):
             break
         time.sleep(1)
 
-# def arm_and_takeoff(vehicle, aTargetAltitude):
-#     """
-#     Arms vehicle and fly to aTargetAltitude.
-#     """
-
-#     print("Basic pre-arm checks")
-#     # Don't try to arm until autopilot is ready
-#     while not vehicle.is_armable:
-#         print(" Waiting for vehicle to initialise...")
-#         time.sleep(1)
-
-#     print("Arming motors")
-#     # Copter should arm in GUIDED mode
-#     vehicle.mode = VehicleMode("GUIDED")
-#     vehicle.armed = True
-
-#     # Confirm vehicle armed before attempting to take off
-#     while not vehicle.armed:
-#         print(" Waiting for arming...")
-#         time.sleep(1)
-
-#     print("Taking off!")
-#     vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
-
-#     # Wait until the vehicle reaches a safe height before processing the goto
-#     #  (otherwise the command after Vehicle.simple_takeoff will execute
-#     #   immediately).
-#     while True:
-#         print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-#         # Break and return from function just below target altitude.
-#         if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
-#             print("Reached target altitude")
-#             break
-#         time.sleep(1)
-
 def flyInSearchPattern(vehicle):
     search_waypoints = load_waypoints_from_csv('generated_search_pattern_waypoints.csv')
     # Iterate over waypoints, expecting lists of [latitude, longitude]
