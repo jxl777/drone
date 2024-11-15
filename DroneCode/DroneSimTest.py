322aea06cad998587197e7d3ca2df72ecdadb751
enorda <enordaUTD@outlook.com>
2024-11-15 14:28:41 -0600

    re-add: Logging

--- DroneCode/DroneSimTest.py
+++ DroneCode/DroneSimTest.py
@@ -93,17 +93,19 @@ def flyInSearchPattern(vehicle):
 
             # Go to the waypoint
             vehicle.simple_goto(wp)
-            currentLoc = (enordaCopter.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon)
+            currentLoc = (vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon)
             currentWP = (wp.lat, wp.lon)
             while(equirectangular_approximation(currentLoc,currentWP)*1000 > .5): 
                 
-                print(f"CurrentLocation: {currentLoc}")
+                print(f"CurrentLocation: ({vehicle.location.global_relative_frame.lat:.12f}, {vehicle.location.global_relative_frame.lon:.12f})")
                 print("Distance:", equirectangular_approximation(currentLoc,currentWP))
                 time.sleep(1)
 
 enordaCopter = connectMyCopter()
 
+print(f"CurrentLocation: ({enordaCopter.location.global_relative_frame.lat:.12f}, {enordaCopter.location.global_relative_frame.lon:.12f})")
 arm_drone(enordaCopter)
+
 waypoints, top_left_corner, top_right_corner, landing_zone_waypoint = run_path_generation(enordaCopter,6,8) #6 and 8 are rough numbers for testing 
 
 print("Set default/target airspeed to 3")
