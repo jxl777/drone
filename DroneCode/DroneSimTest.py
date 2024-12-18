e5ce991ac42fb352ce4e38c095ecf1bb9602f3ce
Rushi1029 <61168374+Rushi1029@users.noreply.github.com>
2024-12-17 23:35:34 -0600

    fix: integrationTest branch Changes

--- DroneCode/DroneSimTest.py
+++ DroneCode/DroneSimTest.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import time
-import logging
 from dronekit import connect, Vehicle, VehicleMode, LocationGlobalRelative
 
 import csv
@@ -8,7 +7,7 @@ import math
 from math import radians, cos, sin, sqrt, atan2, atan, tan
 from SearchAlgoScript import *
 
-
+import multiprocessing
 # Set up option parsing to get connection string
 import argparse
 
@@ -16,37 +15,10 @@ parser = argparse.ArgumentParser(description="Connect to a drone.")
 parser.add_argument("--livedrone", action="store_true", help="Connect to a real drone instead of simulating.")
 args = parser.parse_args()
 
-logger = logging.getLogger('drone_logger')
-logger.setLevel(logging.DEBUG)
-
-# Create a file handler to log to a file
-file_handler = logging.FileHandler('script.log')
-file_handler.setLevel(logging.DEBUG)
-
-# Create a console handler to log to the terminal
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-
-# Create a formatter and set it for both handlers
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-# Add the handlers to the logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
-
 
 # Set SIMULATE_DRONE based on the --livedrone flag
 SIMULATE_DRONE = not args.livedrone  # False if --livedrone is provided, otherwise True
 ALTITUDE = 4
-
-cornerCoordinates = [
-    [1, 32.920440673828125, -96.94830322265625],
-    [2, 32.920440673828125, -96.9479751586914],
-    [3, 32.92019271850586, -96.9479751586914],
-    [4, 32.92019271850586, -96.94831085205078]
-]
 
 # Used to connect to copter with args from command line
 def connectMyCopter():
@@ -81,7 +53,7 @@ def arm_drone(vehicle):
 
     vehicle.armed = True
     while not vehicle.armed:  # While the vehicle has not been armed
-        print("Waiting for drone to arm")
+        print("Waiting for drone to arm: ", {vehicle.system_status.state})
         time.sleep(1)  # Wait one second before checking if drone has been armed
     print("The drone has now been armed")
 
@@ -110,7 +82,7 @@ def getCurrentLocation(vehicle):
     currentLoc = (vehicle.location.global_relative_frame.lat, vehicle.location.global_relative_frame.lon)
     return currentLoc
  
-def flyInSearchPattern(vehicle):
+def flyInSearchPattern(vehicle, location_queue):
     search_waypoints = load_waypoints_from_csv('generated_search_pattern_waypoints.csv')
     # Iterate over waypoints, expecting lists of [latitude, longitude]
     if SIMULATE_DRONE:
@@ -120,9 +92,9 @@ def flyInSearchPattern(vehicle):
             # Go to the waypoint
             vehicle.simple_goto(wp)
             #time.sleep(20)
-            while(equirectangular_approximation(getCurrentLocation(vehicle),currentWP) > .5): 
-            
-                print(f"Current Location: , ({enordaCopter.location.global_relative_frame.lat}, {enordaCopter.location.global_relative_frame.lon})")
+            while(equirectangular_approximation(getCurrentLocation(vehicle),currentWP) > .5):
+                location_queue.put([vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon])
+                print(f"Current Location: , ({vehicle.location.global_relative_frame.lat}, {vehicle.location.global_relative_frame.lon})")
                 print("Distance to WP:", equirectangular_approximation(getCurrentLocation(vehicle),currentWP))
                 time.sleep(1)
     else:
@@ -133,32 +105,24 @@ def flyInSearchPattern(vehicle):
             vehicle.simple_goto(wp)
             #time.sleep(20)
             while(equirectangular_approximation(getCurrentLocation(vehicle),currentWP) > .5): 
-                print(enordaCopter.location.global_relative_frame.alt)
-                print(f"Current Location: , ({enordaCopter.location.global_relative_frame.lat}, {enordaCopter.location.global_relative_frame.lon})")
+                print(vehicle.location.global_relative_frame.alt)
+                location_queue.put([vehicle.location.global_relative_frame.lat,vehicle.location.global_relative_frame.lon])
+                print(f"Current Location: , ({vehicle.location.global_relative_frame.lat}, {vehicle.location.global_relative_frame.lon})")
                 print("Distance to WP:", equirectangular_approximation(getCurrentLocation(vehicle),currentWP))
                 time.sleep(1)
 
-enordaCopter = connectMyCopter()
+# if __name__ == "__main__":
+#     try:
+        
+#         flight_process = multiprocessing.Process(target=drone_control) #, args=(location_queue, )
+#         flight_process.start()
 
-print(f"Starting Location: , ({enordaCopter.location.global_relative_frame.lat}, {enordaCopter.location.global_relative_frame.lon})")
-print("Heading: ", enordaCopter.heading)
+#         # Wait for the processes to finish
 
-logging.critical("ARMING DRONE")
-arm_drone(enordaCopter)
+#         flight_process.join()
 
-waypoints, top_left_corner, top_right_corner, landing_zone_waypoint = run_path_generation(enordaCopter,6,8) #6 and 8 are rough numbers for testing 
+#     except Exception as e:
+#         print(f"Error: {e}")
+#     finally:
 
-print("Set default/target airspeed to 3")
-enordaCopter.airspeed = 3
-
-logging.critical("TAKE OFF")
-takeoff_drone(enordaCopter, 4)
-
-flyInSearchPattern(enordaCopter)
-
-print("Returning to Launch")
-enordaCopter.mode = VehicleMode("RTL")
-
-# Close vehicle object before exiting script
-print("Close vehicle object")
-enordaCopter.close()
+#         print("Closed serial connection.")
